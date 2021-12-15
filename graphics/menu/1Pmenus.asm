6ddd3edb228bc917e2f840a05e0cbca6860549f1
Dimedime-d <3crsd1159@gmail.com>
2021-12-15 00:20:12 -0500

    added more Eng. multiplayer buttons/text

--- graphics/menu/1Pmenus.asm
+++ graphics/menu/1Pmenus.asm
@@ -1,5 +1,13 @@
 
+.org 0x081BC4FC ; replace graphical data in place, without any repointing (takes same space)
+    .incbin "graphics/menu/dumps/random.dmp"
+    
+.org 0x081BC138 ; my binary takes up 0x39B bytes, I have 0x3C4 to work with, I think
+    .incbin "graphics/menu/dumps/bubblenums.dmp"
+
 .autoregion
+    @BubbleNums:
+    .incbin "graphics/menu/dumps/bubblenums.dmp" :: .align
     @Adventure:		
     menu_button_128x16_header
     .incbin "graphics/menu/dumps/1PAdventure.dmp" :: .align
@@ -29,37 +37,52 @@
 		menu_button_160x16_header_partial
 		.incbin "graphics/menu/dumps/1P2PKurukuruKururin.dmp"
 		.word 0x00000000
-		.align 4
+		.align
 	@P3KuruKururin:
 		.byte 0x05,0x80,0x48,0x08
 		menu_button_160x16_header_partial
 		.incbin "graphics/menu/dumps/1P3PKurukuruKururin.dmp"
 		.word 0x00000000
-		.align 4
+		.align
 	@P4KuruKururin:
 		.byte 0x05,0x80,0x48,0x08
 		menu_button_160x16_header_partial
 		.incbin "graphics/menu/dumps/1P4PKurukuruKururin.dmp"
 		.word 0x00000000
-		.align 4
+		.align
 	@P2MinigameParadise:
 		.byte 0x05,0x80,0x48,0x08
 		menu_button_160x16_header_partial
 		.incbin "graphics/menu/dumps/1P2PMinigameParadise.dmp"
 		.word 0x00000000
-		.align 4
+		.align
 	@P3MinigameParadise:
 		.byte 0x05,0x80,0x48,0x08
 		menu_button_160x16_header_partial
 		.incbin "graphics/menu/dumps/1P3PMinigameParadise.dmp"
 		.word 0x00000000
-		.align 4
+		.align
 	@P4MinigameParadise:
 		.byte 0x05,0x80,0x48,0x08
 		menu_button_160x16_header_partial
 		.incbin "graphics/menu/dumps/1P4PMinigameParadise.dmp"
 		.word 0x00000000
-		.align 4
+		.align
+    @FreePlay:
+        menu_button_128x16_header_compressed
+        .incbin "graphics/menu/dumps/MultiFreePlay.dmp"
+        .word 0x00000000
+        .align
+    @RoundLimit:
+        menu_button_128x16_header_compressed
+        .incbin "graphics/menu/dumps/MultiRoundLimit.dmp"
+        .word 0x00000000
+        .align
+    @TimeLimit:
+        menu_button_128x16_header_compressed
+        .incbin "graphics/menu/dumps/MultiTimeLimit.dmp"
+        .word 0x00000000
+        .align
 .endautoregion
 
 ;----------------------
@@ -89,6 +112,10 @@
 	.word @P2MinigameParadise
 	.word @P3MinigameParadise
 	.word @P4MinigameParadise
+.org 0x0802ECB8
+    .word @FreePlay
+    .word @RoundLimit
+    .word @TimeLimit
 .org 0x0802ECC4
 	.word @P2KuruKururin
 	.word @P3KuruKururin
--- graphics/menu/formatmenu.py
+++ graphics/menu/formatmenu.py
@@ -6,7 +6,7 @@ from PIL import Image
 from dataclasses import dataclass
 
 sys.path.append("..")
-from compression import ByBlock, Block
+from compression import LZ, ByBlock, Block
 
 TEMP_FOLDER = "reserve"
 DUMP_FOLDER = "dumps"
@@ -15,13 +15,20 @@ palettes = { # Really just here to convert the .png's easier, in-game handles pa
     "highlighted yellow": [232, 112, 208, 168, 48, 168, 208, 144, 168, 248, 248, 176, 232, 192, 176, 216, 144, 176, 200, 96, 176, 184, 48, 184, 0, 0, 0, 0, 0, 0, 120, 120, 32, 248, 248, 64, 112, 248, 240, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     "highlighted blue": [232, 112, 208, 72, 88, 200, 136, 168, 192, 208, 248, 184, 176, 208, 192, 152, 176, 200, 120, 144, 208, 96, 112, 216, 0, 0, 0, 40, 80, 192, 136, 144, 80, 248, 248, 64, 112, 248, 240, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     "pink": [232, 112, 208, 224, 72, 144, 232, 136, 184, 248, 200, 224, 240, 168, 200, 232, 136, 184, 224, 104, 168, 216, 80, 152, 0, 0, 0, 0, 0, 0, 120, 120, 32, 248, 248, 64, 112, 248, 240, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    "orange": [232, 112, 208, 248, 120, 0, 248, 184, 72, 248, 248, 144, 248, 208, 120, 248, 176, 96, 248, 144, 72, 248, 112, 48, 0, 0, 0, 176, 64, 0, 168, 144, 32, 248, 248, 64, 112, 248, 240, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    "orange": [232, 112, 208, 248, 120, 0, 248, 184, 72, 248, 248, 144, 248, 208, 120, 248, 176, 96, 248, 144, 72, 248, 112, 48, 0, 0, 0, 176, 64, 0, 168, 144, 32, 248, 248, 64, 112, 248, 240, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "challenge highlighted": [0, 128, 152, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 0, 248, 0, 56, 104, 80, 16, 168, 64, 152, 176, 96, 200, 208, 136, 248, 248] #0x1BBB60, decompressed, palette 10
 }
 
-def quantize_to_palette(img, palette, dither=Image.NONE):
-    palImage = Image.new("P", (1, 1))
-    palImage.putpalette(palette + [0, 0, 0] * 240)
-    return img.convert("RGB").quantize(palette=palImage, dither=Image.NONE)
+def quantize_image_to_palette_and_save(img_file, palette):
+        """Quantizes given image to palette, saves and returns path to converted image file"""
+        img: Image.Image = Image.open(img_file)
+        
+        palImage = Image.new("P", (16, 16))
+        palImage.putpalette(palette + [0,0,0]*240)
+        converted_image = img.convert("RGB").quantize(palette=palImage, dither=Image.NONE)
+        converted_file = os.path.join(TEMP_FOLDER, f"{img_file[:-4]}-converted.png")
+        converted_image.save(converted_file)
+        return converted_file
 
 @dataclass(slots=True)
 class MenuButton:
@@ -37,7 +44,7 @@ class MenuButton:
     
     def format(self):
         # Take img file, convert it, grit it, get ref to dmp file
-        converted_file = self.quantize_image_to_palette_and_save(self.img_file, self.palette)
+        converted_file = quantize_image_to_palette_and_save(self.img_file, self.palette)
         bin_file = os.path.join(DUMP_FOLDER, f"{self.img_file[:-4]}")
         # In order: No palette, 4bpp, tile format, NO MAP,
         # 4x2 metatiles, .bin file, no header, output file
@@ -55,18 +62,6 @@ class MenuButton:
                 file.write(comp_data)
             os.remove(f"{bin_file}.img.bin")
         print(f"wrote {self.dmp_file}")
-        
-    @staticmethod
-    def quantize_image_to_palette_and_save(img_file, palette):
-        """Quantizes given image to palette, saves and returns path to converted image file"""
-        img: Image.Image = Image.open(img_file)
-        
-        palImage = Image.new("P", (16, 16))
-        palImage.putpalette(palette + [0,0,0]*240)
-        converted_image = img.convert("RGB").quantize(palette=palImage, dither=Image.NONE)
-        converted_file = os.path.join(TEMP_FOLDER, f"{img_file[:-4]}-converted.png")
-        converted_image.save(converted_file)
-        return converted_file
 
 menu_buttons = [
     MenuButton("1PAdventure.png", "highlighted yellow"),
@@ -82,17 +77,52 @@ menu_buttons = [
     MenuButton("1P3PMinigameParadise.png", "highlighted blue", True),
     MenuButton("1P4PKurukuruKururin.png", "highlighted blue", True),
     MenuButton("1P4PMinigameParadise.png", "highlighted blue", True),
+    MenuButton("MultiFreePlay.png", "highlighted yellow", True),
+    MenuButton("MultiRoundLimit.png", "highlighted yellow", True),
+    MenuButton("MultiTimeLimit.png", "highlighted yellow", True),
 ]
 
 def format_menu_buttons():
     for btn in menu_buttons:
         btn.format()
 
+def format_other_text():
+    # Case by case basis for some others that don't fit nicely into menu buttons...
+
+    # Bubble numbers
+    img_file = "bubblenums.png"
+    converted_file = quantize_image_to_palette_and_save(img_file, palettes["challenge highlighted"])
+    bin_file = os.path.join(DUMP_FOLDER, f"{img_file[:-4]}")
+    # In order: No palette, 4bpp, tile format, NO MAP,
+    # 2x2 metatiles, .bin file, no header, output file
+    os.system(f"cmd /c ..\grit {converted_file} -p! -gB4 -gt -m! -Mw2 -Mh2 -ftb -fh! -o {bin_file}")
+    dmp_file = f"{bin_file}.dmp"
+    comp_data = None
+    with open(f"{bin_file}.img.bin", "rb") as file:
+        comp_data = LZ.compress(file.read())
+    with open(dmp_file, "wb") as file:
+        file.write(comp_data)
+    os.remove(f"{bin_file}.img.bin")
+    print(f"wrote {dmp_file}")
+    
+    #Random Text
+    img_file = "random.png"
+    converted_file = quantize_image_to_palette_and_save(img_file, palettes["challenge highlighted"])
+    bin_file = os.path.join(TEMP_FOLDER, f"{img_file[:-4]}")
+    os.system(f"cmd /c ..\grit {converted_file} -p! -gB4 -gt -m! -Mw4 -Mh2 -aw32 -ftb -fh! -o {bin_file}1")
+    os.system(f"cmd /c ..\grit {converted_file} -p! -gB4 -gt -m! -Mw2 -Mh2 -al32 -ftb -fh! -o {bin_file}2")
+    dmp_file = os.path.join(DUMP_FOLDER, f"{img_file[:-4]}.dmp")
+    with open(f"{bin_file}1.img.bin", "rb") as bin1, open(f"{bin_file}2.img.bin", "rb") as bin2, open(dmp_file, "wb") as dmp:
+        dmp.write(bin1.read())
+        dmp.write(bin2.read())
+    print(f"wrote {dmp_file}")
+    
 def main():
     if not os.path.exists(TEMP_FOLDER):
         os.makedirs(TEMP_FOLDER)
     
     format_menu_buttons()
+    format_other_text()
     
     print("formatted all buttons. check the .asm files if you changed the picture names")
     
