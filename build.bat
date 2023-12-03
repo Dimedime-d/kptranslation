7d6c955ff6471276855f95ab94f7e29fa8d14418
Dimedime-d <3crsd1159@gmail.com>
2023-12-03 01:41:13 -0500

    extract and decompress multiplayer payload

--- build.bat
+++ build.bat
@@ -1,3 +1,17 @@
+@echo off
+
 copy kp.gba kp_patched.gba
-armips.exe main.asm
+
+:: Need to extract multiplayer payload AND decompress it
+:: Replace data with translated images
+:: Then re-compress it to re-insert in the ROM
+:: Need separate armips operations
+mkdir build
+fsutil file createnew build/multipayload1.bin 0
+armips.exe extractmulti.asm
+lzss -d build/multipayload1.bin
+
+
+:: armips.exe main.asm
+
 pause
---
+++ extractmulti.asm
@@ -0,0 +1,6 @@
+.gba
+.open "build/multipayload1.bin", 0x00
+
+.incbin "kp.gba", 0x68c57c, 0x28000
+
+.close
